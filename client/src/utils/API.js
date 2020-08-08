4c277e6dece20bf4fa8ab627045a3e38943a8d4e
Steve S. Lee <sjl558@nyu.edu>
2020-08-08 09:45:07 -0400

    second commit

---
+++ client/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const APIKEY = "x1vEiGNCDZ7dN85mViLU";
+
+
+export default {
+    // Gets all users
+    getindex: function (country) {
+
+        return axios.get("https://www.quandl.com/api/v3/datasets/ECONOMIST/BIGMAC_" + country + "?api_key=x1vEiGNCDZ7dN85mViLU&column_index=3");
+    }
+};
