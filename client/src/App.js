d652f28931d43b5e81e7ccf7084428549e4f672c
Steve S. Lee <sjl558@nyu.edu>
2020-08-11 18:18:50 -0400

    pre-class changes for 8/11

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/pages/Home";
+import Landing from "./components/pages/Landing";
+import Main from "./components/pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/main" component={Main} />
+        <Route exact path="/signout" component={Landing} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
