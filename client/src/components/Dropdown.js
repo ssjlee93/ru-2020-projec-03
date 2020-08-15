ad74154df1501943261a867d369b6ca8a7e2dfa1
Steve S. Lee <sjl558@nyu.edu>
2020-08-15 14:02:26 -0400

    post class commit

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Countries from '../utils/Countries.json';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export default function PocketMoney() {
+    const classes = useStyles();
+    // useState state variable
+    const [countries, setCountries] = useState("");
+    const [importcountries, setImportcountries] = useState(Countries)
+
+    const handleChange = (event) => {
+        let name = event.target.value;
+        setCountries(name)
+    };
+
+    return (
+        <div>
+            <p>This your selection: afadf{countries}</p>
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-native-simple">Countries</InputLabel>
+                <Select
+                    native
+                    value={countries}
+                    onChange={handleChange}
+                >
+                    <option aria-label="None" value=""/>
+                    {Object.entries(importcountries).map(([key,val]) => {
+                        return (
+                            <option key={key} value={val}>{key}</option>
+                        )
+                     })}              
+                    
+                </Select>
+            </FormControl>
+        </div>
+    )
+}
+
+
+// Object.entries(thecountries).map(([key, value])) => (<option value={key}>{value}</option>) 
+// <option  value={val}>{key}</option>
+// <MenuItem key={val} value={val}>{key}</MenuItem>
