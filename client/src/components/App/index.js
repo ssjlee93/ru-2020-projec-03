445c9fd83c3efde6027677dac34e78c3ad3f74a8
Steve S. Lee <sjl558@nyu.edu>
2020-08-18 19:16:32 -0400

    starting app for 8/18

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom'; 
 import Navigation from '../Navigation';
-import Landing from '../Landing';
-import SignUp from '../SignUp';
-import SignIn from '../SignIn';
-import Home from '../Home'; 
+import Landing from '../pages/Landing';
+import Main from '../pages/Main';
+import Home from '../pages/Home'; 
 import * as ROUTES from '../../constants/routes';
  
 const App = () => (
     <Router>
       <div>
         <Navigation />
-   
         <hr />
         <Route exact path="/" component={Landing} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/main" component={Main} />
         <Route exact path="/signout" component={Landing} />
-        <Footer />
       </div>
     </Router>
 
