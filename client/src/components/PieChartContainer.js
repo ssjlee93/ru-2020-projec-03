0e1ab80e2d1e4455b0ffd844825ed43ee300faa4
Kobe Matias <kobematias@yahoo.com>
2020-09-01 18:48:11 -0400

    picchart and sign up style

--- client/src/components/PieChartContainer.js
+++ client/src/components/PieChartContainer.js
@@ -2,22 +2,40 @@ import React from "react";
 import Paper from '@material-ui/core/Paper';
 import Box from "@material-ui/core/Box";
 import Typography from '@material-ui/core/Typography';
-
+import Grid from '@material-ui/core/Grid';
+import Chart from "./Chart";
 export default function PieChartCard(props) {
   return (
-    <div>
-      <Typography variant="h4">Pie Chart Totals</Typography>
+  <div>
+    <Typography variant="h4">Budget Totals & Summary</Typography>
+      <Chart userData={props.userData}/>
+    <Grid container>
+      <Grid item lg={6}>
+        <ul>
+        <li>Administrative</li>
+        <li>Tickets</li>
+        <li>Auto</li>
+        <li>Lodging</li>
+        <li>Food</li>
+        <li>Entertainment</li>
+        <li>Shopping</li>
+        <li>Childcare</li>
+        </ul>
+      </Grid>
+      <Grid item lg={6}>
       <ul>
-      <li>Administrative<span>{props.userData.administrative}</span></li>,
-      <li>Tickets<span>{props.userData.tickets}</span></li>,
-      <li>Auto<span>{props.userData.auto}</span></li>,
-      <li>Lodging<span>{props.userData.lodging}</span></li>,
-      <li>Food<span>{props.userData.food}</span></li>,
-      <li>Entertainment<span>{props.userData.entertainment}</span></li>,
-      <li>Shopping<span>{props.userData.shopping}</span></li>,
-      <li>Childcare<span>{props.userData.childcare}</span></li>,
-      </ul>
-      </div>
+        <li>{props.userData.administrative} USD</li>
+        <li>{props.userData.tickets} USD</li>
+        <li>{props.userData.auto} USD</li>
+        <li>{props.userData.lodging} USD</li>
+        <li>{props.userData.food} USD</li>
+        <li>{props.userData.entertainment} USD</li>
+        <li>{props.userData.shopping} USD</li>
+        <li>{props.userData.childcare} USD</li>
+        </ul>
+      </Grid>
+    </Grid>
+  </div>
   );
 
 }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import { auth, signInWithGoogle, generateUserDocument } from "../utils/firebase";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import "./SignUpStyle.css";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -38,8 +42,11 @@ const SignUp = () => {
   };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign Up</h1>
+    <Grid container className="SignInContainer">
+      <Grid item lg={4}></Grid>
+      <Grid item lg={4} className="SignUpMain">
+        <Card>
+      <Typography variant="h4" className="SignInTitle">Sign Up</Typography>
       <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
         {error !== null && (
           <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
@@ -47,8 +54,9 @@ const SignUp = () => {
           </div>
         )}
 
-        <form noValidate autoComplete="off">
+        <form noValidate autoComplete="off" className="SignInForm">
           <TextField id="displayName"
+            className="SignInTextArea"
             label="Display name"
             variant="filled"
             placeholder="ex: theExample"
@@ -58,6 +66,7 @@ const SignUp = () => {
           <br />
           <TextField 
           id="email" 
+          className="SignInTextArea"
           label="E-mail" 
           variant="filled" 
           name="userEmail" 
@@ -67,21 +76,23 @@ const SignUp = () => {
           <br />
           <TextField 
           id="password" 
+          className="SignInTextArea"
           label="Password" 
           variant="filled"  name="userPassword"
           value={password}
           placeholder="ex: example1234"
           onChange={event => onChangeHandler(event)} />
           <br />
-          <Button variant="outlined" color="primary" onClick={event => {
+          <Button className="CreateAccBtn" variant="outlined" color="primary" onClick={event => {
             createUserWithEmailAndPasswordHandler(event, email, password);
           }}>
             Create Account
             </Button>
+          <br />
         </form>
 
         <p className="text-center my-3">or</p>
-        <button
+        <Button variant="outlined" color="primary"
           onClick={() => {
             try {
               signInWithGoogle();
@@ -92,7 +103,7 @@ const SignUp = () => {
           className="bg-red-500 hover:bg-red-600 w-full py-2 text-white"
         >
           Sign In with Google
-        </button>
+        </Button>
         <p className="text-center my-3">
           Already have an account?{" "}
           <Link to="/" className="text-blue-500 hover:text-blue-600">
@@ -100,7 +111,10 @@ const SignUp = () => {
           </Link>{" "}
         </p>
       </div>
-    </div>
+        </Card>
+      </Grid>
+      <Grid item lg={4}></Grid>
+    </Grid>
   );
 };
 
